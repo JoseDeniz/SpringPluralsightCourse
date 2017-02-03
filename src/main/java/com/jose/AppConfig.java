bcf57c788e9f1d9469e2785b7751bfdce3f729b7
Jose Manuel Deniz Suarez <josedeniz@Joses-MacBook-Pro.local>
2017-02-03 16:46:13 +0000

    Use ComponentScan to autowire beans

--- src/main/java/com/jose/AppConfig.java
+++ src/main/java/com/jose/AppConfig.java
@@ -1,22 +1,10 @@
 package com.jose;
 
-import com.jose.repository.CustomerRepository;
-import com.jose.repository.HibernateCustomerRepository;
-import com.jose.service.CustomerService;
-import com.jose.service.CustomerServiceImpl;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration("main")
+@ComponentScan({"com.jose"})
 public class AppConfig {
 
-    @Bean(name = "customerRepository")
-    public CustomerRepository customerRepository() {
-        return new HibernateCustomerRepository();
-    }
-
-    @Bean(name = "customerService")
-    public CustomerService customerService() {
-        return new CustomerServiceImpl(customerRepository());
-    }
 }
