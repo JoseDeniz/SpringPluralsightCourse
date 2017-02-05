ba2ca54244c26c749c237ac23e0feced0b38b3eb
Jose Manuel Deniz Suarez <josedeniz@Joses-MacBook-Pro.local>
2017-02-05 17:07:14 +0000

    Create a properties file and use it in the repository

--- src/main/java/com/jose/AppConfig.java
+++ src/main/java/com/jose/AppConfig.java
@@ -2,9 +2,11 @@ package com.jose;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration("main")
 @ComponentScan({"com.jose"})
+@PropertySource("app.properties")
 public class AppConfig {
 
 }
--- src/main/java/com/jose/repository/HibernateCustomerRepository.java
+++ src/main/java/com/jose/repository/HibernateCustomerRepository.java
@@ -1,6 +1,7 @@
 package com.jose.repository;
 
 import com.jose.model.Customer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,8 +11,12 @@ import static java.util.Arrays.asList;
 @Repository("customerRepository")
 public class HibernateCustomerRepository implements CustomerRepository {
 
+    @Value("${dbUsername}")
+    private String dbUsername;
+
     @Override
     public List<Customer> findAll() {
+        System.out.println(dbUsername);
         return asList(new Customer("Jose", "Deniz"));
     }
 
